a715e8f0a49d9ad58a5b9c903e10ef6dabad297a
Jason Reeder <info@jasonreeder.com>
2016-07-24 16:52:29 -0700

    Add in the language tags

--- integrate.py
+++ integrate.py
@@ -8,6 +8,7 @@ def parse_decision(lines):
     word = ""
     decision = ""
     line = 0
+
     for i, l in enumerate(lines):
         if l.startswith("** "):
             cases.append((word, decision, line))
@@ -20,6 +21,18 @@ def parse_decision(lines):
             line = lines[i+1]
 
     return cases[1:]
+
+def add_tag(word, language, text):
+    """Add a language tag around a word in a string"""
+
+    tag_open = "<lang script=\"A\" lang=\"%s\">" % (language)
+    tag_close = "</lang>"
+
+    before, after = text.split(word)
+    before += tag_open
+    after = tag_close + after
+
+    return before + word + after
 
 # Parse decisions
 with open("cases.org", "r") as f:
@@ -34,3 +47,19 @@ for u in unique_tags:
     if u not in acceptable_tags:
         print("Warning: the tag %s is unacceptable." % (u))
 
+# Add tags to a new copy of the dictionary
+lines_to_change = [int(d[2]) for d in decisions]
+with open("monier.xml", "r") as f:
+    lines = f.read().splitlines()
+    with open("monier_with_lang_tags.xml", "w") as w:
+        for i, line in enumerate(lines):
+            if i in lines_to_change:
+                w.write(add_tag(d[0], d[1], lines[int(d[2])]))
+            else:
+                w.write(line)
+# with open("monier_with_lang_tags.xml", "w") as f:
+#     pass
+# for d in decisions:
+#     changes.append(add_tag(d[0], d[1], lines[int(d[2])]))
+
+# print(lines_to_change)
