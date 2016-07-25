1dedddd078183f4aae89e627f580bb26ac9fc463
Jason Reeder <info@jasonreeder.com>
2016-07-24 18:16:01 -0700

    Properly handle doubles
    There are many lines that have more than one word in them. This change
    ensures that these lines are not duplicated.

--- integrate.py
+++ integrate.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from collections import defaultdict
 
 # Helper functions
 def parse_decision(lines):
@@ -12,7 +13,7 @@ def parse_decision(lines):
 
     for i, l in enumerate(lines):
         if l.startswith("** "):
-            cases.append((word, decision, line))
+            cases.append((word, decision, int(line)))
             word = l[8:]
             decision = ""
             line = 0
@@ -35,8 +36,10 @@ def add_tag(word, language, text):
 
     return before + word + after
 
+# Run functions
 def parse_decisions_doc():
     """Run function parse_decisions() on input file"""
+    global decisions
 
     with open("cases.org", "r") as f:
         lines = f.read().splitlines()
@@ -55,28 +58,42 @@ def test_tags():
 def create_complete_dictionary_file():
     """Add tags to a new copy of the dictionary
        This is very large and isn't a good way of testing"""
+    # TODO: Refactor this to use the output of create_changes_file() below
 
-    lines_to_change = [int(d[2]) for d in decisions]
+    lines_to_change = [d[2] for d in decisions]
     with open("monier.xml", "r") as f:
         lines = f.read().splitlines()
         with open("monier_with_lang_tags.xml", "w") as w:
             for i, line in enumerate(lines):
                 if i in lines_to_change:
-                    w.write(add_tag(d[0], d[1], lines[int(d[2])]) + "\n")
+                    w.write(add_tag(d[0], d[1], lines[d[2]]) + "\n")
                 else:
                     w.write(line + "\n")
 
 def create_changes_file():
     """Add tags to a special "changes" file"""
 
-    changes = []
+    changes = defaultdict(list)
+    lines_to_change = [d[2] for d in decisions]
+    doubles = set([l for l in lines_to_change if lines_to_change.count(l) > 1])
+    decisions_with_doubles = [d for d in decisions if d[2] in doubles]
+    decisions_without_doubles = [d for d in decisions if d[2] not in doubles]
+    assert len(decisions) == len(decisions_with_doubles) + len(decisions_without_doubles)
+
     with open("monier.xml", "r") as f:
         lines = f.read().splitlines()
         for d in decisions:
-            changes.append(add_tag(d[0], d[1], lines[int(d[2])]))
+            if d[2] in changes:
+                changes[d[2]] = add_tag(d[0], d[1], changes[d[2]])
+            else:
+                changes[d[2]] = add_tag(d[0], d[1], lines[d[2]])
+    # pprint(changes)
 
     with open("changes.xml", "w") as f:
-        f.write("\n".join(changes))
+        for k,v in sorted(changes.items()):
+            f.write(str(k) + "\n")
+            f.write("".join(v) + "\n\n")
+        # f.write("\n".join(changes))
 
 def run():
     parse_decisions_doc()
