aed54dc6fff43b07fd812a45852da16945a1e6fe
Jason Reeder <info@jasonreeder.com>
2016-07-24 18:29:16 -0700

    Clean up unused code

--- integrate.py
+++ integrate.py
@@ -1,9 +1,8 @@
-from pprint import pprint
 from collections import defaultdict
 
 # Helper functions
 def parse_decision(lines):
-    """Returns a list of the information neede to add lang tags:
+    """Return a list of the information neede to add lang tags:
        the word, it's language tag, and the line"""
 
     cases = []
@@ -29,7 +28,6 @@ def add_tag(word, language, text):
 
     tag_open = "<lang script=\"A\" lang=\"%s\">" % (language)
     tag_close = "</lang>"
-
     before, after = text.split(word)
     before += tag_open
     after = tag_close + after
@@ -39,6 +37,7 @@ def add_tag(word, language, text):
 # Run functions
 def parse_decisions_doc():
     """Run function parse_decisions() on input file"""
+
     global decisions
 
     with open("cases.org", "r") as f:
@@ -51,35 +50,17 @@ def test_tags():
 
     acceptable_tags = ['Hindustani', 'Turkish', 'Arabic', 'Persian']
     unique_tags = set([d[1] for d in decisions])
+
     for u in unique_tags:
         if u not in acceptable_tags:
             print("Warning: the tag %s is unacceptable." % (u))
 
-def create_complete_dictionary_file():
-    """Add tags to a new copy of the dictionary
-       This is very large and isn't a good way of testing"""
-    # TODO: Refactor this to use the output of create_changes_file() below
-
-    lines_to_change = [d[2] for d in decisions]
-    with open("monier.xml", "r") as f:
-        lines = f.read().splitlines()
-        with open("monier_with_lang_tags.xml", "w") as w:
-            for i, line in enumerate(lines):
-                if i in lines_to_change:
-                    w.write(add_tag(d[0], d[1], lines[d[2]]) + "\n")
-                else:
-                    w.write(line + "\n")
-
-def create_changes_file():
-    """Add tags to a special "changes" file"""
+def create_output_file():
+    """Add tags to a special output file"""
 
     changes = defaultdict(list)
-    lines_to_change = [d[2] for d in decisions]
-    doubles = set([l for l in lines_to_change if lines_to_change.count(l) > 1])
-    decisions_with_doubles = [d for d in decisions if d[2] in doubles]
-    decisions_without_doubles = [d for d in decisions if d[2] not in doubles]
-    assert len(decisions) == len(decisions_with_doubles) + len(decisions_without_doubles)
 
+    # Prepare dictionary of changes
     with open("monier.xml", "r") as f:
         lines = f.read().splitlines()
         for d in decisions:
@@ -87,18 +68,16 @@ def create_changes_file():
                 changes[d[2]] = add_tag(d[0], d[1], changes[d[2]])
             else:
                 changes[d[2]] = add_tag(d[0], d[1], lines[d[2]])
-    # pprint(changes)
 
-    with open("changes.xml", "w") as f:
+    # Output to file
+    with open("monier_lines_with_tags.xml", "w") as f:
         for k,v in sorted(changes.items()):
             f.write(str(k) + "\n")
             f.write("".join(v) + "\n\n")
-        # f.write("\n".join(changes))
 
-def run():
+def run()
     parse_decisions_doc()
     test_tags()
-    # create_complete_dictionary_file()
-    create_changes_file()
+    create_output_file()
 
 run()
